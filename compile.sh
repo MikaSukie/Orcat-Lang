9adcfa864f00723d7218977c2042d9a8976804db
!MikaLorielle(미카수키)! <75727770+MikaLorielle@users.noreply.github.com>
2025-06-12 04:00:01 +0200

    Create compile.sh

---
+++ compile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+
+start=$(date +%s%3N)
+
+./ORCC main.orcat -o out.ll
+
+end=$(date +%s%3N)
+
+elapsed=$((end - start))
+elapsed_s=$((elapsed / 1000))
+elapsed_ms=$((elapsed % 1000))
+
+echo "Elapsed time: ${elapsed_s}.${elapsed_ms} seconds"
