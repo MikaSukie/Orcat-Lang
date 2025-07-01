fedb1dec08d494937f1622f8f7601252a9fc59e5
!MikaLorielle(미카수키)! <75727770+MikaLorielle@users.noreply.github.com>
2025-07-01 19:54:46 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 clear
 
-start=$(date +%s%3N)
-
-./ORCC.bin main.orcat -o out.ll
-
-end=$(date +%s%3N)
-
-elapsed=$((end - start))
-elapsed_s=$((elapsed / 1000))
-elapsed_ms=$((elapsed % 1000))
-
-echo "Elapsed time: ${elapsed_s}.${elapsed_ms} seconds"
+time ORCC.bin main.orcat -o out.ll
