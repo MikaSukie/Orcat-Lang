7da612a5e97c5a8e0e297a207300d130d8518f6c
!MikaLorielle(미카수키)! <75727770+MikaLorielle@users.noreply.github.com>
2025-07-03 14:07:58 +0200

    Update Rcompile.sh

--- Rcompile.sh
+++ Rcompile.sh
@@ -3,7 +3,7 @@ clear
 
 time bash -c '
   ./ORCC.bin main.orcat -o out.ll &&
-  clang -Wno-override-module out.ll stdlib.c -o ocat &&
-  ./ocat
+  clang -Wno-override-module out.ll stdlib.c -o main &&
+  ./main
   echo "Exit code: $?"
 '
