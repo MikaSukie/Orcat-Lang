7d82da17a92d31926971bd4ee41974075ac9919e
!MikaLorielle(미카수키)! <75727770+MikaSukie@users.noreply.github.com>
2025-11-21 15:29:00 +0100

    Add Nuitka command with console mode disabled
    Added a command to run Nuitka with console mode disabled.

--- compile_compiler.ps1
+++ compile_compiler.ps1
@@ -1,3 +1,4 @@
 cd C:\Users\USER\Documents\Projects\OrcatTest\
 python -m nuitka --onefile --windows-icon-from-ico=orcc.ico ORCC.py
 . .\.venv\Scripts\Activate.ps1
+python -m nuitka --onefile --windows-console-mode=disable --windows-icon-from-ico=orcc.ico ORCC.py
